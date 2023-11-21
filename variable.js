076e2a74d8a04c0e1384ce11474860bc88d37f0c
kunal <kunalkumbhar1278@gmail.com>
2023-11-21 09:47:16 +0530

    intro variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+//  Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically(var-default)
+// Using var
+// Using let
+// Using const
+
+// let/var/const variable Name = value;
+var mark = 79;
+console.log(mark);
