2557eae398eb825feba3dcd2e6b31f614f76c9ce
kunal <kunalkumbhar1278@gmail.com>
2023-12-24 20:07:49 +0530

    component creation

--- AngularProject01/myApp01/src/app/app.component.ts
+++ AngularProject01/myApp01/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myApp01';
+  a = 20;
+  c!: number; // ! <-- this is used for declaration of property and assining value later
 }
